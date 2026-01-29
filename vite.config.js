6a6f17d2f7835f5eff600c0ea4eac1ea867252b3
Wiratata <wiranata.glitch@gmail.com>
2026-01-29 12:41:24 -0700

    Update build and config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-website/', // 👈 REQUIRED for GitHub Pages
+  base: '/portfolio-website/', // must match repo name
   plugins: [react(), tailwindcss()],
-})
+});
